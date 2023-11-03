4e5e2c0d6ff5679b21022d782a30396b9646e9a9
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2023-11-03 15:13:49 -0400

    Time: 59 ms (30.19%), Space: 44.3 MB (22.5%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
@@ -4,49 +4,43 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    /* get shortest string */
-    let gcd = ""
-    if (str1.length >= str2.length){
-        gcd = str2;
-    } else{
-        gcd = str1;
-    }
+        //set ans to the smallest string
+        let ans = "";
+        if (str1.length >= str2.length){
+            ans = str2;
+        }else{
+            ans = str1;
+        }
 
-    /* test GCD */
-    function testGCD(str){
-        let ans = true;
-
-        if (str.length % gcd.length === 0){
+        //divide string
+        const divide = (str) => {
+            let bool = true
             let cur = 0;
-            let sub = str.substring(cur, cur+gcd.length)
-            while(sub.length){
-                if (sub === gcd){
-                    cur += gcd.length;
-                    sub = str.substring(cur, cur+gcd.length);
-                    continue;
+            let sub = str.substring(cur, cur+ans.length);
+            while(sub.length > 0){
+                if (sub === ans){
+                    cur += ans.length;
+                    sub = str.substring(cur, cur+ans.length);
                 }else{
-                    ans = false
+                    bool = false;
                     break;
                 }
             }
-        }else{
-            return false
+            return bool;
         }
 
-        /* return a boolean */
-        return ans;
-    }
-
-    /* search for GCD */
-    while(gcd.length > 0){
-        if (testGCD(str1) && testGCD(str2)){
-            break;
-        }else{
-            /* set gcd to length minus 1 */
-            gcd = gcd.substring(0, gcd.length-1)
+        while(ans.length > 0){
+            if (
+                str1.length % ans.length === 0 && 
+                str2.length % ans.length === 0 &&
+                divide(str1) &&
+                divide(str2)
+                ){
+                    break;
+                }else{
+                    ans = ans.substring(0, ans.length-1);
+                }
         }
-    }
-
-    /* return GCD */
-    return gcd;
+    
+            return ans;
 };
