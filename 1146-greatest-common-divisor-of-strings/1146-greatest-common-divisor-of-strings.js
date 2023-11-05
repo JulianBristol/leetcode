e31341d52d13b9a6d23844e785305f6b475cc8df
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2023-11-05 11:13:04 -0500

    Time: 59 ms (30.51%), Space: 42.6 MB (44.49%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
@@ -4,40 +4,23 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    //find the smallest string
-    let ans = '';
-    if (str1.length >= str2.length){
-        ans = str2;
-    }else{
-        ans = str1;
-    }
+    /* Check if strings have a GCD */
+    if (str1+str2 !== str2+str1) return "";
 
-    //divide the strings
-    const divide = (str) => {
-        let bool = true;
-        let cur = 0;
-        let sub = str.substring(cur, cur+ ans.length)
-        while(sub !== ''){
-            if (sub === ans){
-                cur+= ans.length;
-                sub = str.substring(cur, cur+ ans.length);
-                continue;
-            }else{
-                bool = false;
-                break;
-            }
+    /* turn strings into numbers to
+    Get the GCD using numbers */
+    let a = str1.length;
+    let b = str2.length;
+    let temp = b
+
+    while (a % temp || b % temp){
+        if (a % temp){
+            temp -= (a % temp)
         }
-        return bool;
-    }
-
-    //determine if ans is the gcd
-    while(ans.length > 0){
-        if (str1.length % ans.length === 0 && str2.length % ans.length === 0 && divide(str1) && divide(str2)){
-            break;
-        }else{
-            ans = ans.substring(0,ans.length -1)
+        else{
+            temp -= (b % temp)
         }
     }
 
-    return ans;
+    return str2.substring(0,temp);
 };
