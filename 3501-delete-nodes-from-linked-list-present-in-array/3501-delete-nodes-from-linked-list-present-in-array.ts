d9f16705f8e225f698df58c7d2b35bcfa642312b
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-09-06 03:47:58 -0400

    Time: 391 ms (13.4%), Space: 95 MB (53.61%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function modifiedList(nums: number[], head: ListNode | null): ListNode | null {
+    const set = new Set();
+    nums.map((x) => {
+        set.add(x);
+    })
+
+    let prev = new ListNode(0,head);
+    const ans = head;
+
+    while (head && head.next){
+        if (set.has(head.val)){
+            head.val = head.next.val;
+            head.next = head.next.next;
+        } else {
+            head = head.next;
+            prev = prev.next;
+        }
+    }
+    if (set.has(head.val)){
+        prev.next = prev.next.next;
+    }
+    return ans;
+};
