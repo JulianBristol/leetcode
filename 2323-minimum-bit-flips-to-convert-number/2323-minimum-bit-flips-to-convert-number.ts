1ccaaf761c745a3cef1b0d58d76bc9b7736b9bad
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-09-11 16:04:36 -0400

    Time: 61 ms (28%), Space: 51.8 MB (20%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.ts
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.ts
@@ -1,51 +1,20 @@
 function minBitFlips(start: number, goal: number): number {
-    //convert both to binary
-    let startBin = start.toString(2);
-    let goalBin = goal.toString(2);
-    console.log(startBin, goalBin)
+  //convert both to binary
+  let startBin = start.toString(2);
+  let goalBin = goal.toString(2);
 
-    //get number of number places
-    let numPlaces = Math.max(startBin.length, goalBin.length);
+  //get number of differences
+  goalBin = goalBin.padStart(startBin.length, "0");
+  startBin = startBin.padStart(goalBin.length, "0");
 
-    //Append zeros to smaller number
-    let startBigger = false;
-    if (startBin.length >= goalBin.length){
-        startBigger = true;
+  //count differences
+  let ans = 0;
+  for (let i = 0; i < startBin.length; i++) {
+    if (startBin.charAt(i) !== goalBin.charAt(i)) {
+      ans++;
     }
+  }
 
-    if (startBigger){
-        const additionalZeros = startBin.length - goalBin.length;
-        let additional = "";
-        for (let i = 0; i < additionalZeros; i++){
-            additional += "0"
-        }
-        goalBin = additional + goalBin;
-    } else {
-        const additionalZeros = goalBin.length - startBin.length;
-        let additional = "";
-        for (let i = 0; i < additionalZeros; i++){
-            additional += "0"
-        }
-        startBin = additional + startBin;
-    }
-    console.log(startBin, goalBin)
-
-    //count differences
-    let ans = 0;
-    for (let i = 0; i < startBin.length; i++){
-        console.log(startBin.charAt(i), goalBin.charAt(i))
-        if (startBin.charAt(i) !== goalBin.charAt(i)){
-            ans++;
-        }
-    }
-    
-    //return answer
-    return ans;
-    /* convert both to binary
-get number of leading zeros
-match leading zeros for both
-see where they don't match, count the missing matches,
-return missing matches */
-
-
-};
+  //return answer
+  return ans;
+}
