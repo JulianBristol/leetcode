50f21c3122e9ab372108e31e5228cb050e630145
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-09-27 02:17:03 -0400

    Time: 2086 ms (56.08%), Space: 156.4 MB (78.38%) - LeetHub

--- 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.ts
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.ts
@@ -12,47 +12,47 @@ function sumPrefixScores(words: string[]): number[] {
     class Trie {
         root: TrieNode;
 
-        constructor (){
+        constructor(){
             this.root = new TrieNode();
         }
 
-        insert(word: string): void {
+        insert(word: string): void{
             let current = this.root;
             for(const ch of word){
                 if (!current.children[ch]){
                     current.children[ch] = new TrieNode();
                 }
-                current = current.children[ch]
+                current = current.children[ch];
                 current.score++;
             }
         }
 
-        countScore(word: string): number {
-            let count = 0;
+        getScores(word: string): number{
             let current = this.root;
-            for(const ch of word){
+            let count = 0;
+            for (const ch of word){
                 current = current.children[ch];
                 count += current.score;
             }
-
             return count;
         }
     }
 
     let ans = [];
 
-    const sumScores = (trie: Trie, words: string[]) => {
-        for (const word of words){
-            trie.insert(word);
-        }
-        for(const word of words){
-            ans.push(trie.countScore(word))
-        }
-    }
-
     const trie = new Trie();
 
-    sumScores(trie, words)
+    const placeScores = ((words: string[], trie: Trie):void => {
+        for(const word of words){
+            trie.insert(word);
+        }
+
+        for (const word of words){
+            ans.push(trie.getScores(word));
+        }
+    })
+
+    placeScores(words, trie)
 
     return ans;
 };
