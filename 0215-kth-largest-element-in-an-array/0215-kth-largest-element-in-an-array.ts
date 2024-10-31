55a8dcb5cd767ecc653fa2a0b95739a7a85e46e2
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-10-30 21:57:12 -0400

    Time: 157 ms (18.14%), Space: 70.1 MB (28.78%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.ts
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.ts
@@ -1,77 +1,62 @@
 function findKthLargest(nums: number[], k: number): number {
     class MinHeap {
-        //add the properties of the class
-        //need an array to store the values (in order) inside the heap
-        heap: number[];
-        //max size of the heap - Used so we can ensure our heap never gets too large
         size: number;
+        heap: number[];
 
-        //now we need a constructor. A constructor is a base case for object. It states the structure of your object
-        constructor(size: number, heap?: number[]){
+        constructor(size: number, heap?: number[]) {
             this.size = size;
-            if (!heap){
-                this.heap = [];
-            } else{
+            if (heap){
                 this.heap = heap;
+            } else{
+                this.heap = [];
             }
         }
 
-        //Ensures the heap is maintained when adding values to the heap
-        //when an element is added, it ensures that the heap is in order
         bubbleUp(): void{
-            //start from the last index of the heap
             let index = this.heap.length -1;
-            while (index > 0){
-                let parentIndex = Math.floor((index - 1)/2);
-                if (this.heap[index] < this.heap[parentIndex]){
-                    //didn't know I could do this
-                    [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
-                    //move to a higher location on the tree
-                    index = parentIndex;
-                } else {
-                    //Heap order is satisfied
-                    break;
+
+            while(index > 0){
+                let parent = Math.floor((index -1)/2);
+                if (this.heap[index] < this.heap[parent]){
+                    [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]]
+                    index = parent;
+                } else{
+                    break
                 }
             }
         }
 
-        //Ensures the heap is maintained when removing values from the heap
-        //checks if the new root is greater than it's children and if swaps with the smallest child
         bubbleDown(): void{
             let index = 0;
-            let length = this.heap.length
-            while (index < length){
-                const left = 2*index + 1;
-                const right = 2*index + 2;
-                let smallestIndex = index;
+            let length = this.heap.length;
 
-                //if left is smaller than the smallest index we have, left becomes the new smallest index
-                if (left < length && this.heap[left] < this.heap[smallestIndex]){
-                    smallestIndex = left;
+            while(index < length){
+                let smallestVal = index;
+                let left = 2 * index + 1;
+                let right = 2 * index + 2;
+
+                if (left < length && this.heap[left] < this.heap[smallestVal]){
+                    smallestVal = left;
+                }
+                if (right < length && this.heap[right] < this.heap[smallestVal]){
+                    smallestVal = right;
                 }
 
-                //if right is smaller than left and is the smallest index we have, right becomes the new smallest index
-                if (right < length && this.heap[right] < this.heap[smallestIndex]){
-                    smallestIndex = right;
-                }
-
-                //if we just changed the smallest index, we need to swap the parent with the children and continue downwards
-                if (smallestIndex !== index){
-                    [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]]
-                    index = smallestIndex; //move down the tree
+                if (index !== smallestVal){
+                    [this.heap[index], this.heap[smallestVal]] = [this.heap[smallestVal], this.heap[index]];
+                    index = smallestVal;
                 } else{
                     break;
                 }
             }
         }
 
-        //add values to the heap
-        add(value: number):void {
+        add(val: number): void{
             if (this.heap.length < this.size){
-                this.heap.push(value);
+                this.heap.push(val);
                 this.bubbleUp();
-            } else if (value > this.heap[0]){
-                this.heap[0] = value;
+            } else if (this.heap[0] < val){
+                this.heap[0] = val;
                 this.bubbleDown();
                 this.bubbleUp();
             }
@@ -79,8 +64,9 @@ function findKthLargest(nums: number[], k: number): number {
     }
 
     const minHeap = new MinHeap(k);
+
     nums.forEach((x) => {
-        minHeap.add(x)
+        minHeap.add(x);
     })
 
     return minHeap.heap[0];
