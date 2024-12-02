72a3f0aa22878dd546b50a99864b165865a8830e
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-01 22:25:42 -0500

    Time: 63 ms (90.74%), Space: 69.3 MB (75%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.ts
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.ts
@@ -1,103 +1,116 @@
 class MinHeap {
-    heap: [number, number][] = [];
+  private heap: number[] = [];
 
-    constructor() {}
+  constructor() {}
 
-    peek(): [number, number] | undefined {
-        return this.heap[0];
+  peek(): number {
+    return this.heap[0];
+  }
+
+  add(num: number): void {
+    this.heap.push(num);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  delete(): number | undefined {
+    if (this.heap.length === 0) return undefined;
+    const pop = this.heap[0];
+    const last = this.heap.pop()!;
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.bubbleDown(0);
+    }
+    return pop;
+  }
+
+  bubbleUp(currentIndex: number = this.heap.length - 1): void {
+    let parent = Math.floor((currentIndex - 1) / 2);
+
+    while (currentIndex > 0) {
+      if (this.heap[currentIndex] < this.heap[parent]) {
+        this.swap(currentIndex, parent);
+        currentIndex = parent;
+        parent = Math.floor((currentIndex - 1) / 2);
+      } else break;
+    }
+  }
+
+  bubbleDown(currentIndex: number = 0): void {
+    let smallest = currentIndex;
+    let left = 2 * currentIndex + 1;
+    let right = 2 * currentIndex + 2;
+
+    if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+      smallest = left;
     }
 
-    add(num: [number, number]): void {
-        this.heap.push(num);
-        this.bubbleUp(this.heap.length - 1);
+    if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+      smallest = right;
     }
 
-    delete(): [number, number] | undefined {
-        if (this.heap.length === 0) return undefined;
-
-        const pop = this.heap[0];
-        const last = this.heap.pop()!;
-        if (this.heap.length > 0) {
-            this.heap[0] = last;
-            this.bubbleDown(0);
-        }
-        return pop;
+    if (smallest !== currentIndex) {
+      this.swap(smallest, currentIndex);
+      this.bubbleDown(smallest);
     }
+  }
 
-    bubbleUp(index: number): void {
-        let parent = Math.floor((index - 1) / 2);
-        while (index > 0 && this.heap[index][0] < this.heap[parent][0]) {
-            [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]];
-            index = parent;
-            parent = Math.floor((index - 1) / 2);
-        }
-    }
-
-    bubbleDown(index: number): void {
-        let left = 2 * index + 1;
-        let right = 2 * index + 2;
-        let smallest = index;
-
-        if (left < this.heap.length && this.heap[left][0] < this.heap[smallest][0]) {
-            smallest = left;
-        }
-        if (right < this.heap.length && this.heap[right][0] < this.heap[smallest][0]) {
-            smallest = right;
-        }
-        if (smallest !== index) {
-            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-            this.bubbleDown(smallest);
-        }
-    }
+  swap(idx1: number, idx2: number): void {
+    [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+  }
 }
 
 function totalCost(costs: number[], k: number, candidates: number): number {
-    const leftHeap = new MinHeap();
-    const rightHeap = new MinHeap();
-    let leftPointer = 0;
-    let rightPointer = costs.length - 1;
-    let ans = 0;
+  //create heaps
+  const left = new MinHeap();
+  const right = new MinHeap();
+  let leftPointer = 0;
+  let rightPointer = costs.length - 1;
+  let ans = 0;
 
-    // Add the initial candidates to the heaps
-    for (let i = 0; i < candidates; i++) {
-        if (leftPointer <= rightPointer) {
-            leftHeap.add([costs[leftPointer], leftPointer]);
-            leftPointer++;
-        }
-        if (leftPointer <= rightPointer) {
-            rightHeap.add([costs[rightPointer], rightPointer]);
-            rightPointer--;
-        }
+  //allocate workers to heaps
+  for (let i = 0; i < candidates; i++) {
+    if (leftPointer <= rightPointer) {
+      left.add(costs[leftPointer]);
+      leftPointer++;
     }
 
-    // Hire k workers
-    while (k > 0) {
-        let leftMin = leftHeap.peek();
-        let rightMin = rightHeap.peek();
-
-        if (!leftMin && !rightMin) break;
-
-        if (rightMin && (!leftMin || rightMin[0] < leftMin[0])) {
-            const worker = rightHeap.delete();
-            if (worker) {
-                ans += worker[0];
-                if (rightPointer >= leftPointer) {
-                    rightHeap.add([costs[rightPointer], rightPointer]);
-                    rightPointer--;
-                }
-            }
-        } else if (leftMin) {
-            const worker = leftHeap.delete();
-            if (worker) {
-                ans += worker[0];
-                if (leftPointer <= rightPointer) {
-                    leftHeap.add([costs[leftPointer], leftPointer]);
-                    leftPointer++;
-                }
-            }
-        }
-        k--;
+    if (leftPointer <= rightPointer) {
+      right.add(costs[rightPointer]);
+      rightPointer--;
     }
+  }
 
-    return ans;
+  //hire k workers
+  while (k > 0) {
+    let rightMin = right.peek();
+    let leftMin = left.peek();
+
+    if (!rightMin && !leftMin) break;
+
+    if (rightMin && (!leftMin || rightMin < leftMin)) {
+      const worker = right.delete();
+      if (worker) {
+        ans += worker;
+
+        if (leftPointer <= rightPointer) {
+          right.add(costs[rightPointer]);
+          rightPointer--;
+        }
+      }
+    } else if (leftMin) {
+      const worker = left.delete();
+
+      if (worker) {
+        ans += worker;
+
+        if (leftPointer <= rightPointer) {
+          left.add(costs[leftPointer]);
+          leftPointer++;
+        }
+      }
+    }
+    k--;
+  }
+
+  return ans;
 }
