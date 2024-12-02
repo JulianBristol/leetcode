f71e326cbcaf06a0a447e1a9c759c823ecfb44e6
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-02 13:50:42 -0500

    Time: 214 ms (57.5%), Space: 81.6 MB (96.36%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.ts
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.ts
@@ -1,89 +1,91 @@
-class MinHeap {
-    private maxSize: number;
-    private heap: number[];
+class MinHeap{
+    private heap: number[][] = [];
 
-    constructor(k: number) {
-        this.maxSize = k;
-        this.heap = [];
+    constructor() {}
+
+    add(num: number[]): void{
+        this.heap.push(num);
+        this.bubbleUp(this.heap.length -1);
     }
 
-    add(num: number): void {
-        let heap = this.heap;
-        heap.push(num);
-        this.bubbleUp(heap.length -1)
-    }
-
-    remove(): number | undefined{
+    delete(): number[]|undefined{
         if (this.heap.length === 0) return undefined;
+
         const pop = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.bubbleDown();
+        const last = this.heap.pop()!;
+        if (this.heap.length > 0){
+            this.heap[0] = last;
+            this.bubbleDown();
+        }
 
         return pop;
     }
 
-    bubbleUp(currentIndex: number): void{
-        const heap = this.heap;
+    bubbleUp(currentIndex: number = this.heap.length -1): void{
         let parent = Math.floor((currentIndex - 1)/2);
-
-        while(currentIndex > 0){
-            if (heap[parent] > heap[currentIndex]){
-                [heap[parent], heap[currentIndex]] = [heap[currentIndex], heap[parent]]
-                currentIndex = parent;
-                parent = Math.floor((currentIndex - 1)/2);
-            } else{
-                break;
-            }
+        
+        while (currentIndex > 0 && this.heap[currentIndex][0] < this.heap[parent][0]){
+            this.swap(currentIndex, parent);
+            currentIndex = parent;
+            parent = Math.floor((currentIndex -1)/2);
         }
     }
 
-    bubbleDown(): void{
-        let heap = this.heap;
-        let currentIndex = 0;
+    bubbleDown(currentIndex: number = 0): void{
+        const left = 2 * currentIndex + 1;
+        const right = 2 * currentIndex + 2;
         let smallest = currentIndex;
 
-        while (currentIndex < heap.length){
-            let left = (currentIndex * 2) + 1;
-            let right = (currentIndex * 2) + 2;
-
-            if (left < heap.length && heap[left] < heap[smallest]){
-                smallest = left;
-            }
-            if (right < heap.length && heap[right] < heap[smallest]){
-                smallest = right;
-            }
-
-            if (smallest !== currentIndex){
-                [heap[smallest], heap[currentIndex]] = [heap[currentIndex], heap[smallest]];
-                currentIndex = smallest;
-            } else{
-                break;
-            }
+        if (left < this.heap.length && this.heap[left][0] < this.heap[smallest][0]){
+            smallest = left;
         }
+        if (right < this.heap.length && this.heap[right][0] < this.heap[smallest][0]){
+            smallest = right;
+        }
+        if (smallest !== currentIndex){
+            this.swap(currentIndex, smallest);
+            this.bubbleDown(smallest);
+        }
+    }
+
+    swap(idx: number, idx1: number): void{
+        [this.heap[idx], this.heap[idx1]] = [this.heap[idx1], this.heap[idx]]
     }
 
     getSize(): number{
         return this.heap.length;
     }
+
+    peek(): number[]{
+        return this.heap[0];
+    }
 }
+
 function maxScore(nums1: number[], nums2: number[], k: number): number {
-    let tuple = nums1.map((elem, index) => [elem, nums2[index]]).sort((a,b) => b[1] - a[1])
+    //create tuples
+    const tuples = nums1.map((x, index) => [x, nums2[index]]).sort((a,b) => b[1]-a[1]);
+    // create heap
+    const heap = new MinHeap;
     let ans = 0;
-    const heap = new MinHeap(k);
     let sum = 0;
 
-    for (let i = 0; i < nums1.length; i++){
-        heap.add(tuple[i][0]);
-        sum += tuple[i][0];
+    for (let i = 0; i < nums2.length; i++){
+        //add to sum
+        sum += tuples[i][0];
+        //populate the heap
+        heap.add(tuples[i]);
 
-        if (heap.getSize() > k){
-            sum -= heap.remove();
+        //ensure there are only k elements
+        if(heap.getSize() > k){
+            sum -= heap.delete()[0];
         }
 
+        //get max score
         if (heap.getSize() === k){
-            ans = Math.max(ans, sum*tuple[i][1]);
+            let multiplier = tuples[i][1];
+            ans = Math.max(ans, sum * multiplier);
         }
-        
+
     }
     return ans;
 };
