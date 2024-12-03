d4e7928978bbd7d0aac495c57da3ddd7c97dde05
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-02 21:20:53 -0500

    Time: 140 ms (7.02%), Space: 83.7 MB (6.82%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
@@ -8,31 +8,29 @@ function successfulPairs(spells: number[], potions: number[], success: number): 
     let ans = [];
 
     for (let i = 0; i < spells.length; i ++){
-        let mult = spells[i];
-
         //find the failpoint/passPoint
          let min = 0, max = potions.length-1, mid = Math.floor(potions.length/2);
 
          while(true){
             //no failpoint (potions[0] >= success)
-            if (mid === 0 && mult * potions[mid] >= success){
+            if (mid === 0 && spells[i] * potions[mid] >= success){
                 ans.push(potions.length);
                 break;
             }
             //no passpoint (potions[last] < success)
-            if (mid === potions.length -1 && mult * potions[mid] < success){
+            if (mid === potions.length -1 && spells[i] * potions[mid] < success){
                 ans.push(0);
                 break;
             }
                 //mid above failpoint
-            if(mult * potions[mid] >= success){
+            if(spells[i] * potions[mid] >= success){
                 max = mid -1;
             } else {
                 //mid below failpoint
                 min = mid +1;
             }
             //yes failpoint (num < success, num+1 >= success)
-            if (mult*potions[mid] < success && mult * potions[mid+1] >= success){
+            if (spells[i] *potions[mid] < success && spells[i] * potions[mid+1] >= success){
                 ans.push(potions.length-1 - mid)
                 break;
             }
