e126b78d9367670925abf92ef26c67e21c56ec05
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-02 22:40:18 -0500

    Time: 0 ms (100%), Space: 51.3 MB (77.67%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.ts
+++ 0162-find-peak-element/0162-find-peak-element.ts
@@ -1,5 +1,23 @@
 function findPeakElement(nums: number[]): number {
-    if (nums.length === 1) return 0;
-    const tuples = nums.map((x, index) => [x, index]).sort((a,b) => b[0]-a[0]);
-    return tuples[0][1];
+    let left = 0;
+    let right = nums.length -1;
+    let peak = 0;
+
+    while(left < right){
+        const mid = left + ((right -left) >> 1);
+
+        if ((nums[mid] > nums[mid-1] || !nums[mid-1]) && (nums[mid] > nums[mid+1] || !nums[mid+1])) return mid;
+
+        else if (nums[mid-1] && nums[mid -1] > nums[mid]){
+            peak = mid-1;
+            right = mid -1;
+            continue;
+        } else if (nums[mid+1] && nums[mid +1] > nums[mid]){
+            peak = mid+1;
+            left = mid+1;
+            continue;
+        }
+    }
+
+    return peak;
 };
