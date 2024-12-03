dc18df8bb7f1ce170d8ecabed1198eec0f3854d4
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-02 21:55:57 -0500

    Time: 74 ms (91.81%), Space: 80.3 MB (78.18%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
@@ -1,48 +1,18 @@
 function successfulPairs(spells: number[], potions: number[], success: number): number[] {
-    //sort potions
-    //find the failpoint (point where the next potion passes and current fails)
-    //add rest of potions to ans
-
-    //sort potions
     potions.sort((a,b) => a-b);
-    let ans = [];
-    const map = new Map();
 
-    for (let i = 0; i < spells.length; i ++){
-        if (map.has(spells[i])){
-            ans.push(map.get(spells[i]));
-            continue;
+    return spells.map( s => {
+        let min = 0;
+        let max = potions.length;
+
+        while (min < max){
+            const mid = min + ((max - min) >> 1);
+            if (s * potions[mid] >= success){
+                max = mid;
+            } else{
+                min = mid + 1;
+            }
         }
-        //find the failpoint/passPoint
-         let min = 0, max = potions.length-1, mid = Math.floor(potions.length/2);
-
-         while(true){
-            //no failpoint (potions[0] >= success)
-            if (mid === 0 && spells[i] * potions[mid] >= success){
-                ans.push(potions.length);
-                break;
-            }
-            //no passpoint (potions[last] < success)
-            if (mid === potions.length -1 && spells[i] * potions[mid] < success){
-                ans.push(0);
-                break;
-            }
-                //mid above failpoint
-            if(spells[i] * potions[mid] >= success){
-                max = mid -1;
-            } else {
-                //mid below failpoint
-                min = mid +1;
-            }
-            //yes failpoint (num < success, num+1 >= success)
-            if (spells[i] *potions[mid] < success && spells[i] * potions[mid+1] >= success){
-                ans.push(potions.length-1 - mid)
-                break;
-            }
-            mid = Math.floor((max+min)/2);
-         }
-         map.set(spells[i], ans[ans.length-1]);
-    }
-
-    return ans;
+        return potions.length - min;
+    })
 };
