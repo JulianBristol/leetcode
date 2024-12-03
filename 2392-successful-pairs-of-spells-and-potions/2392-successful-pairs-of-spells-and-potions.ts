4d34e82a83a11bdb7857977cc83f798ebb34edc5
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-02 21:16:56 -0500

    Time: 152 ms (5.26%), Space: 84.9 MB (5.46%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
@@ -0,0 +1,46 @@
+function successfulPairs(spells: number[], potions: number[], success: number): number[] {
+    //sort potions
+    //find the failpoint (point where the next potion passes and current fails)
+    //add rest of potions to ans
+
+    //sort potions
+    potions.sort((a,b) => a-b);
+    let ans = [];
+
+    for (let i = 0; i < spells.length; i ++){
+        let mult = spells[i];
+
+        //find the failpoint/passPoint
+         let min = 0, max = potions.length-1, mid = Math.floor(potions.length/2);
+
+         while(true){
+            //no failpoint (potions[0] >= success)
+            if (mid === 0 && mult * potions[mid] >= success){
+                ans.push(potions.length);
+                break;
+            }
+            //no passpoint (potions[last] < success)
+            if (mid === potions.length -1 && mult * potions[mid] < success){
+                ans.push(0);
+                break;
+            }
+            //yes failpoint (num < success, num+1 >= success)
+            if (mult*potions[mid] < success && mid+1 < potions.length && mult * potions[mid+1] >= success){
+                ans.push(potions.length-1 - mid)
+                break;
+            }
+                //mid above failpoint
+            if(mult * potions[mid] >= success){
+                max = mid -1;
+                mid = Math.floor((max+min)/2);
+            } else {
+                //mid below failpoint
+                min = mid +1;
+                mid = Math.floor((max+min)/2);
+            }
+
+         }
+    }
+
+    return ans;
+};
