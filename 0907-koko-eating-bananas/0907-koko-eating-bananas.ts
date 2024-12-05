368b110a3a6003976e40b24a72dcf8ffcbd07230
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-05 17:16:18 -0500

    Time: 8 ms (85.82%), Space: 53.7 MB (38.76%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.ts
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.ts
@@ -21,10 +21,12 @@ function minEatingSpeed(piles: number[], h: number): number {
             counter -= Math.ceil(piles[i]/mid);
             if (counter < 0) {
                 min = mid+1;
+                break;
             }
             if (counter >= 0 && i === piles.length-1){
-                ans = Math.min(ans, mid)
+                ans = mid
                 max = mid -1;
+                break;
             }
         }
     }
