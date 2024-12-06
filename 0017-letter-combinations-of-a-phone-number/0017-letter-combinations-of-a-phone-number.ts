7b167a26c1843e73a58cb2a72bf7377f95e653c1
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2024-12-05 23:28:38 -0500

    Time: 0 ms (100%), Space: 51.7 MB (23.36%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
@@ -1,20 +1,28 @@
-const letters = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+const digitToChar = new Map()
+    digitToChar.set('2', 'abc')
+    digitToChar.set('3', 'def')
+    digitToChar.set('4', 'ghi')
+    digitToChar.set('5', 'jkl')
+    digitToChar.set('6', 'mno')
+    digitToChar.set('7', 'pqrs')
+    digitToChar.set('8', 'tuv')
+    digitToChar.set('9', 'wxyz')
+
 function letterCombinations(digits: string): string[] {
     if (digits.length === 0) return []
     //take the digits, pull out the first - that is what we work on
     //recurse the rest of the digits into letterCombinations
     //with your current 3 or 4 digits, append them at the end of each other
-    let ans:string[] = [];
-    let digit = Number(digits.charAt(0));
+    let ans = [];
+    let digit = digits.charAt(0);
     digits = digits.slice(1);
-    for (let letter of letters[digit - 2].split("")){
-        let string = letter;
+    for (let letter of digitToChar.get(digit)){
         let chars = letterCombinations(digits);
         if (chars.length === 0){
             ans.push(letter);
         }else {
             for(let char of chars){
-                ans.push(string.concat(char))
+                ans.push(letter.concat(char))
             }
         }
     }
