cfbdc0edfbf00879d7870f2e359cecd0a64d9ab0
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-09 18:13:09 -0500

    Time: 12 ms (44.82%), Space: 54.2 MB (21.31%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.ts
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.ts
@@ -1,37 +1,26 @@
 function minEatingSpeed(piles: number[], h: number): number {
-    //the binary search we need to search through is all the numbers from 1 to the pile's max value
-    //therefore, we need to get the pile's max
     let min = 1;
-    let max = Math.max(...piles);
+    let max = 0;
 
-    //we need to figure out the minimum eating speed for the piles and the hours
-    //this means that we need to find the eating speed that passes just before it fails;
-    //this is an iterative process
-
-    //to do this, we need to subtract the mid from the piles till h <= 0
-    //then take the min of all the passing values
+    for (const pile of piles){
+        max = Math.max(max, pile);
+    }
     let ans = max;
 
-    //loop through all numbers between min and max
-    while(min <= max){
-        let counter = h;
-        let mid = Math.floor((min + max)/2);
-        //loop through every pile in piles[]
-        for (let i = 0; i < piles.length; i++){
-            counter -= Math.ceil(piles[i]/mid);
-            if (counter < 0) {
-                min = mid+1;
-                break;
-            }
-            if (counter >= 0 && i === piles.length-1){
-                ans = mid
-                max = mid -1;
-                break;
-            }
+    while (min <= max){
+        const mid = Math.floor((min + max)/2);
+        let hours = h;
+        for (const pile of piles){
+            hours -= Math.ceil(pile/mid);
+        }
+
+        if (hours < 0){
+            min = mid+1;
+        } else{
+            max = mid-1;
+            ans = Math.min(ans, mid)
         }
     }
 
-
-    //return the min eating speed
     return ans;
 };
