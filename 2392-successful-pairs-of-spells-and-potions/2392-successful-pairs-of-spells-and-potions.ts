c5c505906a0c51f18028c295030c30bd3966b748
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-11 16:04:21 -0500

    Time: 109 ms (42.53%), Space: 78.3 MB (97.29%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.ts
@@ -1,25 +1,22 @@
 function successfulPairs(spells: number[], potions: number[], success: number): number[] {
     potions.sort((a,b) => a-b);
-
-    let ans = [];
+    const ans = [];
 
     for (const spell of spells){
-        let max = potions.length -1;
         let min = 0;
-        let minMatch = potions.length;
+        let max = potions.length -1;
 
-        while (min <= max){
-            let cur = Math.floor((min + max)/2);
-            let res = spell * potions[cur];
+        while(min <= max){
+            const cur = Math.floor((min + max)/2);
+            const res = spell * potions[cur];
 
             if (res >= success){
-                minMatch = cur;
-                max = cur-1;
-            } else{
-                min = cur +1
+                max = cur - 1;
+            } else {
+                min = cur + 1;
             }
         }
-        ans.push(potions.length - minMatch)
+        ans.push(potions.length - min)
     }
 
     return ans;
