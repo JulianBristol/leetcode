842d5d5b1c683b78726da89a4505a42cfe246023
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-11 19:01:05 -0500

    Time: 35 ms (5.2%), Space: 54.9 MB (6.21%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.ts
@@ -1,30 +1,29 @@
-const letters = new Map()
-    letters.set('2', 'abc')
-    letters.set('3', 'def')
-    letters.set('4', 'ghi')
-    letters.set('5', 'jkl')
-    letters.set('6', 'mno')
-    letters.set('7', 'pqrs')
-    letters.set('8', 'tuv')
-    letters.set('9', 'wxyz')
-
 function letterCombinations(digits: string): string[] {
     if (digits.length === 0) return []
-    //take the digits, pull out the first - that is what we work on
-    //recurse the rest of the digits into letterCombinations
-    //with your current 3 or 4 digits, append them at the end of each other
-    let ans = [];
-    let digit = digits.charAt(0);
-    digits = digits.slice(1);
-    for (let letter of letters.get(digit)){
-        let chars = letterCombinations(digits);
-        if (chars.length === 0){
-            ans.push(letter);
-        }else {
-            for(let char of chars){
-                ans.push(letter.concat(char))
-            }
+    const ans = [];
+    const letters = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+    const firstLetter = digits.substring(0,1);
+    const allLetters = digits.slice(1);
+    
+    const letArray = letters[Number(firstLetter) - 2].split("")
+
+    letArray.forEach((letter) => {
+        console.log(allLetters)
+        const smallerLetters = letterCombinations(allLetters)
+        if (smallerLetters.length > 0){
+            smallerLetters.forEach((smallLetter = "") => {
+            console.log("smallerletter: ", smallLetter)
+            let addedLetter = letter + smallLetter;
+            ans.push(addedLetter)
+        })
+        } else {
+            ans.push(letter)
         }
-    }
-    return ans;
+        
+            
+        
+    })
+
+    return ans
 };
