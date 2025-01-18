b028c8310341885cb1f5b3d7b303bef38e1b1d12
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-18 18:58:05 -0500

    Time: 2 ms (46.39%), Space: 51.8 MB (55.42%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.ts
+++ 0216-combination-sum-iii/0216-combination-sum-iii.ts
@@ -1,34 +1,25 @@
 function combinationSum3(k: number, n: number): number[][] {
-    let result: number[][] = [];
+    const ans: number[][] = [];
 
-    //function for backtracking
-    const backtrack = (k: number, n: number, start: number, currentCombination: number[]) => {
-        //if k is exactly 0 while n is zero, that means we hit a valid combo and it
-        //should be saved
-        if (k===0 && n === 0){
-            result.push([...currentCombination])
+    const backtract = (k: number, n: number, start: number, combination: number[]) => {
+        if (k === 0 && n === 0){
+            ans.push([...combination]);
             return;
         }
 
-        //if n < 0 or k === 0 then there is nothing we can do so return
         if (k === 0 || n <= 0){
-            return
+            return;
         }
 
-        //Try all numbers recursively
         for (let i = start; i >= 1; i--){
-            //Add current number to the combination
-            currentCombination.push(i);
+            combination.push(i);
 
-            //Recursively add the next digits, subtracting k and n
-            backtrack(k - 1, n - i, i - 1, currentCombination)
+            backtract(k-1, n-i, i-1, combination);
 
-            //now that that number is done, lets remove it and try the next combination
-            currentCombination.pop();
+            combination.pop();
         }
+
     }
-
-    backtrack(k, n, 9, []);
-
-    return result;
+    backtract(k, n, 9, []);
+    return ans;
 };
