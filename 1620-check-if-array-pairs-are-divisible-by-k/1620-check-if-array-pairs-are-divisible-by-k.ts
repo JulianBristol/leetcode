309357ef0dfbd693fd98b401ad3d71e478ceb26e
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-19 16:16:51 -0500

    Time: 59 ms (21.43%), Space: 70.3 MB (7.14%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.ts
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.ts
@@ -1,24 +1,42 @@
 function canArrange(arr: number[], k: number): boolean {
-    if (arr[0] === -1014622 || arr[0] === 84312452) return true;
-    const set = new Set<number>();
+    const map = new Map<number, number>();
+    map.set(0,0)
 
-    for (let i = 0; i < arr.length; i++){
-        if (!set.has(i)){
-            for(let j = i+1; j < arr.length; j++){
-                if (set.has(j)){
-                    continue;
-                } else if ((arr[i] + arr[j]) % k === 0){
-                    set.add(i)
-                    set.add(j)
-                    break;
-                } else if (j === arr.length-1){
-                    return false;
-                }
-            }
+    for( let num of arr){
+        const remainder = ((num % k)+k)%k;
+        if (!map.has(remainder)){
+            map.set(remainder, 0);
+        }
+        map.set(remainder, map.get(remainder) + 1);
+    }
+
+    if (k%2 === 0){
+        const midpoint = map.get(k/2);
+        if (midpoint !== undefined && midpoint%2 !== 0){
+            return false;
+        }else{
+            map.delete(k/2)
         }
     }
 
-    if (set.size === arr.length){
-        return true;
-    } else return false;
+    if  (map.get(0)%2 !== 0){
+        return false;
+    } else{
+        map.delete(0)
+    }
+    
+let ans = true
+
+console.log(map)
+    map.forEach((value, key) => {
+        const opp = (k - key);
+        if (map.get(opp) !== undefined && map.get(opp) === value){
+            map.delete(opp)
+            map.delete(key)
+        }else{
+            ans = false;
+        }
+    })
+
+    return ans;
 };
