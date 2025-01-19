05e85112c255a381b2fbe4ec2d0352b3f4bb8ab6
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-19 17:06:34 -0500

    Time: 70 ms (7.14%), Space: 71.6 MB (7.14%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.ts
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.ts
@@ -3,30 +3,19 @@ function canArrange(arr: number[], k: number): boolean {
 
     for(let r of arr){
         const remainder = ((r%k)+k)%k;
-        let opp = k - remainder;
-
-        if (!map.has(remainder)){
-            map.set(remainder, 0);
+        
+        //check if there is a complement remainder
+        const complement = (k-remainder) %k;
+        if (map.has(complement) && map.get(complement)! >0){
+            //pair found, decrement complement
+            map.set(complement, map.get(complement)-1);
+            //if complement === 0 delete it
+            if (map.get(complement) === 0) map.delete(complement);
+        } else{
+            //add remainder if complement not found
+            map.set(remainder, (map.get(remainder) || 0) + 1);
         }
-        if (!map.has(opp)){
-            map.set(opp, 0);
-        }
-
-        map.set(remainder, map.get(remainder)+1);
-        if (remainder !== 0) map.set(opp, map.get(opp)-1);
     }
-
-    if (map.get(0) && map.get(0)%2 !== 0){
-        return false;
-    } else{
-        map.delete(0)
-    }
-
-    map.forEach((value, key) => {
-        if (value === 0){
-            map.delete(key);
-        }
-    })
 
     return map.size === 0;
 };
