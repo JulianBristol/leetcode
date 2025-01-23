4aeaf0bd9e80b9586abe8d83d076d0ddad66a516
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-23 00:52:49 -0500

    Time: 2 ms (73.61%), Space: 51.5 MB (87.5%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.ts
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.ts
@@ -1,7 +1,4 @@
 function numTilings(n: number): number {
-    if (n === 1) return 1;
-    if (n === 2) return 2; 
-
     const MOD = 1_000_000_007;
     //memoization to store the results for subproblems
     const dp: number[] = new Array(n +1).fill(0);
@@ -9,18 +6,6 @@ function numTilings(n: number): number {
     dp[0] = 1
     dp[1] = 1
     dp[2] = 2
-
-    const makeState = (t1,t2) => {
-        if (!t1){
-            if (!t2){
-                return 0;
-            }
-            return 2;
-        } else if (t2){
-            return 3
-        }
-        return 1
-    }
 
     for (let i = 3; i <= n; i++){
         dp[i] = (2 * dp[i-1] + dp[i-3]) %MOD;
