bc5c3d9f28475989969e1e403fc0745347abf80e
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-25 17:22:32 -0500

    Time: 49 ms (84.82%), Space: 76.2 MB (43.16%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.ts
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.ts
@@ -1,14 +1,14 @@
 function longestCommonSubsequence(text1: string, text2: string): number {
-    const dp: number[][] = Array.from({ length: text1.length +1 }, () => Array(text2.length+1).fill(0))
-    for(let x = 1; x <= text1.length; x++){
-        for (let y = 1; y <= text2.length; y++){
-            if (text1[x-1] === text2[y-1]){
+    const dp: number[][] = Array.from({ length: text1.length + 1}, () => Array(text2.length+1).fill(0))
+
+    for (let x = 1; x <= text1.length; x++){
+        for(let y = 1; y <= text2.length; y++){
+            if(text1[x-1] == text2[y-1]){
                 dp[x][y] = dp[x-1][y-1] + 1;
             } else{
-                dp[x][y] = Math.max(dp[x][y-1],dp[x-1][y])
+                dp[x][y] = Math.max(dp[x][y-1],dp[x-1][y]);
             }
         }
     }
-
-    return dp[text1.length][text2.length];
+    return dp[text1.length][text2.length]
 };
