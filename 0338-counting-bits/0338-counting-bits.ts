8823e06f8ff6299cbb400af441a9deef03f2a5cd
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-25 01:30:20 -0500

    Time: 4 ms (55.83%), Space: 57.1 MB (60.19%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.ts
+++ 0338-counting-bits/0338-counting-bits.ts
@@ -2,7 +2,13 @@ function countBits(n: number): number[] {
     const ans = [];
 
     for(let i = 0; i <= n; i++){
-        ans.push((i.toString(2)).split('0').join('').length)
+        let count = 0;
+        let n = i
+        while(n > 0){
+            n &= (n-1);
+            count++;
+        }
+        ans.push(count)
     }
 
     return ans;
