d140e63ac4d5170a472e844779d0e7ed30b01f42
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-25 20:07:14 -0500

    Time: 3 ms (79.05%), Space: 58.9 MB (67.57%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.ts
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.ts
@@ -1,10 +1,9 @@
 function maxProfit(prices: number[], fee: number): number {
     let profit = 0;
-
     let hold = -prices[0];
 
-    for (let i = 1; i < prices.length; i++){
-        profit = Math.max(profit, hold + prices[i] -fee);
+    for(let i = 1; i <prices.length; i++){
+        profit = Math.max(profit, hold + prices[i] - fee);
         hold = Math.max(hold, profit - prices[i])
     }
 
