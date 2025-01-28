8e7795397fba138a8879769ac48391d2f4ddf4bc
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-28 01:31:40 -0500

    Time: 51 ms (88.62%), Space: 51 MB (65.85%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.ts
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.ts
@@ -1,23 +1,24 @@
 function minFlips(a: number, b: number, c: number): number {
-    let max =  Math.max(c, a|b)
     let ans = 0;
 
-    for (let i = 0; i < max.toString(2).length; i++){
-        if(((a >> i) & 1) | ((b >> i) & 1)){
-            if (((c >> i) & 1)){
-                continue;
+    while (a > 0 || b > 0 || c > 0){
+        let bitA = a & 1;
+        let bitB = b & 1;
+        let bitC = c & 1;
+
+        if (bitC === 0){
+            if (bitA | bitB){
+                ans += bitA + bitB;
             }
-            //one or both == 1
-            if (((a >> i) & 1) & ((b >> i) & 1)){
-                ans += 2;
-            } else{
-                ans++;
+        } else{
+            if ((bitA|bitB) === 0){
+                ans += 1
             }
-        } else if (((c >> i) & 1)){
-            //a|b = 0 but c = 1
-            ans++;
         }
-        //else do nothing and move to next bit
+
+        a >>= 1;
+        b >>= 1;
+        c >>= 1;
     }
 
     return ans;
