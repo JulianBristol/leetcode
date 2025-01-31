216a883cedcc776424902ca08abf12a7e6dd870e
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-30 22:51:50 -0500

    Time: 23 ms (67.69%), Space: 73.6 MB (48.83%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -1,11 +1,15 @@
 function dailyTemperatures(temperatures: number[]): number[] {
+    //create an array that is temperatures.length long
     const ans = new Array(temperatures.length).fill(0);
+    //create an array for the stack (which is a stack of the day (not temp), so
+    //it is a stack of temperatures indexes)
     const stack = [];
 
     for (let i = 0; i < temperatures.length; i++){
         if (stack.length === 0 || temperatures[stack[stack.length-1]] > temperatures[i]){
             stack.push(i);
-        } else{
+        } else {
+            //stack[last] < temp[i]
             while(stack.length > 0 && temperatures[stack[stack.length-1]] < temperatures[i]){
                 ans[stack[stack.length-1]] = i - stack.pop();
             }
