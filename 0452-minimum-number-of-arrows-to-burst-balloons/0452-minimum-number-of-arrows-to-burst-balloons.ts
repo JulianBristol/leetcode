333828dbe4a799356750c4b14b941268e996a94a
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-01-30 20:26:05 -0500

    Time: 90 ms (63.23%), Space: 73.8 MB (41.24%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.ts
@@ -0,0 +1,19 @@
+function findMinArrowShots(points: number[][]): number {
+    points.sort((a,b) => a[1]-b[1])
+
+    let start = points[0][0];
+    let end = points[0][1];
+    let ans = 0;
+
+    for(let i = 1; i < points.length; i++){
+        if (end < points[i][0]){
+            ans++;
+            start = points[i][0]
+            end = points[i][1];
+        } else if (end > points[i][1]){
+            end = points[i][1];
+        }
+    }
+
+    return ans+1;
+};
