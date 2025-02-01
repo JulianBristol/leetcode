5553b57522acc809b48a7b667c7a3843848b20f4
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-02-01 03:27:30 -0500

    Time: 0 ms (100%), Space: 51.6 MB (89.29%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.ts
+++ 3429-special-array-i/3429-special-array-i.ts
@@ -1,6 +1,6 @@
 function isArraySpecial(nums: number[]): boolean {
     for (let i = 1; i < nums.length; i++){
-        if ((nums[i-1] & 1) === (nums[i] & 1)){
+        if (!((nums[i-1] + nums[i]) & 1)){
             return false;
         }
     }
