53fcb1afef7e2139eaa65774c3ced6c22736f148
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-02-02 11:58:57 -0500

    Time: 64 ms (32.23%), Space: 52.2 MB (40.31%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -1,12 +1,8 @@
 function mergeAlternately(word1: string, word2: string): string {
     let ans = ""
     for (let i = 0; i < Math.max(word1.length, word2.length); i++){
-        if (word1.length > i){
-            ans += word1[i]
-        }
-        if (word2.length > i){
-            ans += word2[i]
-        }
+        if (word1.length > i) ans += word1[i]
+        if (word2.length > i) ans += word2[i]
     }
     return ans;
 };
