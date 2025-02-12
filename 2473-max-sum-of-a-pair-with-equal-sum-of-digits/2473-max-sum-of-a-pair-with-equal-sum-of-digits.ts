4c5b346366f706ab00c4c8b663c02ab7a5192229
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-02-12 00:44:30 -0500

    Time: 61 ms (56.25%), Space: 61.8 MB (91.25%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.ts
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.ts
@@ -2,10 +2,17 @@ function maximumSum(nums: number[]): number {
     const map = new Map<number, number>();
     let ans = -1;
 
+    function digitSum(n: number): number {
+        let sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = Math.floor(n / 10);
+        }
+        return sum;
+    }
+
     for(const num of nums){
-        const sum = num.toString().split("").reduce((sum, digit) => {
-            return sum + Number(digit)
-        }, 0)
+        const sum = digitSum(num)
 
         if (map.has(sum)){
             ans = Math.max(ans, map.get(sum)+ num);
