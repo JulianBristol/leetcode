d7c5c714eb689992dec0210d2070b2daf5883a2f
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-03 16:17:56 -0500

    Time: 2 ms (68.79%), Space: 60.9 MB (15.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.ts
+++ 0055-jump-game/0055-jump-game.ts
@@ -1,19 +1,23 @@
 function canJump(nums: number[]): boolean {
-    /* 
-    create counter
-    create for loop
-    for each item in the loop, counter = math.max(counter, item)
-    counter--
-    if counter = 0 return false !!!!May need to move the decrement to before Math.max execution
+    let furthestJump = nums[0];
 
-    return true;
+    for(let i = 0; i < nums.length; i++){
+        furthestJump = Math.max(furthestJump, i + nums[i]);
+        if(furthestJump >= nums.length -1){
+            return true;
+        } else if (i === furthestJump){
+            break;
+        }
+    }
+    
+    return false;
+    /**
+    
+create furthestJump variable = 0;
+for each item in nums
+	furthestJump = Math.max(furthestJump, i + nums[i])
+	if (furthestJump >= nums.length -1) return true;
+	
+return false;
      */
-     let counter = nums[0];
-     for (let i = 1; i < nums.length; i++){
-        counter--;
-        if (counter === -1 ) return false;
-        counter = Math.max(counter, nums[i]);
-     }
-
-     return true;
 };
