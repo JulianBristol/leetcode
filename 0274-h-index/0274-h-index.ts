e83a875c61e14ab9842fd0ff712df847a4075f5a
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-20 23:05:41 -0400

    Time: 1 ms (79.28%), Space: 54.9 MB (39.66%) - LeetHub

--- 0274-h-index/0274-h-index.ts
+++ 0274-h-index/0274-h-index.ts
@@ -1,14 +1,14 @@
 function hIndex(citations: number[]): number {
-    citations.sort((a,b) => a-b);
+    citations.sort((a, b) => a - b);
     
     let ans = 0;
 
-    for (let i = 0; i < citations.length; i++){
+    for (let i = 0; i < citations.length; i++) {
         let numCitations = citations.length - i;
-        if (citations[i] >= numCitations){
-            return numCitations
+        if (citations[i] >= numCitations) {
+            return numCitations;
         }
     }
 
-    return ans
-};
+    return ans;
+}
