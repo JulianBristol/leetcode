a1faee5f2e27d40c158dfac3c2c0bc7d8a645f30
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-24 13:49:58 -0400

    Time: 4 ms (60.4%), Space: 58.2 MB (26.2%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map<number, number>();
+
+    for( let i = 0; i < nums.length; i++){
+        const res = target - nums[i];
+        if(map.has(res)){
+            return [map.get(res), i]
+        } else{
+            map.set(nums[i], i)
+        }
+    }
+};
