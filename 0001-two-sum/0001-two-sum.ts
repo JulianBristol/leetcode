ba9033b80a20cb6c5f61533eb57af725819dde8d
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-24 14:04:53 -0400

    Time: 2 ms (81.09%), Space: 57.9 MB (30.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,15 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
-    const seen = new Set<number>();  // Store numbers we've already seen.
+    const set = new Set<number>();
 
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++){
         const complement = target - nums[i];
-        
-        if (seen.has(complement)) {
-            return [nums.indexOf(complement), i];  // Return the indices of the two numbers.
-        }
 
-        seen.add(nums[i]);  // Add current number to the set for future lookups.
+        if (set.has(complement)){
+            return [nums.indexOf(complement), i];
+        } else{
+            set.add(nums[i]);
+        }
     }
 
-    return [];  // If no solution found, return empty array (optional for cases where there's no valid solution).
-}
+    return [];
+};
