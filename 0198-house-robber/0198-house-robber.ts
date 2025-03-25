89bdc0c04ab74153763e8ff954cd1f2ed610f372
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-25 12:42:17 -0400

    Time: 0 ms (100%), Space: 53.9 MB (55.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -1,16 +1,17 @@
 function rob(nums: number[]): number {
-    if (nums.length === 1){
-        return nums[0];
+    //solve for edge cases n = 1|2|3
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
+    if (nums.length === 3) return Math.max(nums[0] + nums[2], nums[1]);
+    //create an array for memoization and store the values for the max when n = 1, 2, or 3
+    const memoizedArr = [nums[0], Math.max(nums[0], nums[1]), Math.max(nums[0] + nums[2], nums[1])]
+
+    //create a for loop that starts at n=3 and what I need to do at each stage is to add the current value of n to
+    for(let i = 3; i < nums.length; i++){
+        let temp = Math.max(memoizedArr[i-3], memoizedArr[i-2]);
+        memoizedArr.push(temp + nums[i])
     }
+    //array n-3 and do the same to array n-2
 
-    let prev1 = 0;
-    let prev2 = 0;
-
-    for (let num of nums){
-        let temp = prev1;
-        prev1 = Math.max(prev1, prev2+num);
-        prev2 = temp
-    }
-
-    return prev1
+    return Math.max(memoizedArr[memoizedArr.length-1], memoizedArr[memoizedArr.length-2])
 };
