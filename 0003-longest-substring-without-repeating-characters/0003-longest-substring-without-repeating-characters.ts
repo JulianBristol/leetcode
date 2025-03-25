6a6dd1b1184ac0ddba9debb85966735b9283f22c
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-24 20:15:44 -0400

    Time: 7 ms (70.62%), Space: 58.7 MB (52.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -1,4 +1,6 @@
 function lengthOfLongestSubstring(s: string): number {
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
     //create a hashmap of chars
     const map = new Map<string, number>();
     //create a two pointer keeping track of my start and stop locations
