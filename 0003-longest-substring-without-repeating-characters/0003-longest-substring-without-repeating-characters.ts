90f779f871d0ca8a8a735d822febb6467998615e
Julian Bristol <82460914+JulianBristol@users.noreply.github.com>
2025-03-24 20:12:52 -0400

    Time: 6 ms (78.81%), Space: 58.4 MB (55.78%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -0,0 +1,28 @@
+function lengthOfLongestSubstring(s: string): number {
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+    //create a hashmap of chars
+    const map = new Map<string, number>();
+    //create a two pointer keeping track of my start and stop locations
+    let start = 0;
+    let stop = 0;
+    let ans = 1;
+
+    //create a for loop where I check for each character in the hashmap.
+    while(stop < s.length){
+        map.set(s[stop], map.get(s[stop]) ? map.get(s[stop]) +1 : 1)
+        while (map.get(s[stop]) > 1){
+            //move the left pointer to the right by 1
+            map.set(s[start], map.get(s[start]) -1);
+            start = start+1;
+        }
+
+        ans = Math.max(ans, stop - start +1)
+        stop++;
+    }
+    //if it exists, move my left pointer to the right by 1 and check again
+    //if it does not exist take the max of ans and stop - start+1
+    //return ans
+
+    return ans;
+};
